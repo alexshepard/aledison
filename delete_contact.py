94beb154c786c2b79ae79ec3dc4fe5a492ae1e2b
Alex Shepard <alex@inaturalist.org>
2015-09-06 20:04:29 -0700

    delete contact operation

---
+++ delete_contact.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+import yaml
+config = yaml.safe_load(open("config.yml"))
+
+from contacts import Contacts, Contact
+c = Contacts()
+
+# syntax: add_contact.py <name> <number>
+import sys
+
+if len(sys.argv) < 2:
+	print("usage: delete_contact.py <name>")
+	sys.exit()
+
+script_name = sys.argv.pop(0)
+name = sys.argv.pop(0)
+
+contact = c.find_contact_by_name(name)
+if contact:
+	c.delete_contact(contact)
+else:
+	print("no contact named " + name)
+	sys.exit()
