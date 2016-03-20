1a6089f67bc34c6e0943ff560231001d39f89321
Alex Shepard <alex@inaturalist.org>
2016-03-20 13:03:17 -0700

    use twilio message filtering api

--- print_texts_with.py
+++ print_texts_with.py
@@ -35,17 +35,24 @@ def name_or_number(number):
     return number
 
 if contact:
-  messages = twilio_client.messages.list()
-  for message in reversed(twilio_client.messages.list()):
+  in_messages = twilio_client.messages.list(
+    _from = contact.number
+  )
+  out_messages = twilio_client.messages.list(
+    to = contact.number
+  )
+  filtered_messages = in_messages + out_messages
+  filtered_messages.sort(key=lambda msg: msg.date_sent)
+
+  for message in filtered_messages:
     msg_from = message.from_.encode("utf-8")
     msg_to = message.to.encode("utf-8")
-    if msg_from == contact.number or msg_to == contact.number:
-      print("From: " + name_or_number(msg_from))
-      print("To: " + name_or_number(msg_to))
-        # twilio messages are naive utc dates
-      utc_date_sent = utc.localize(message.date_sent)
-      local_date_sent = utc_date_sent.astimezone(my_timezone)
-      print("Date: " + local_date_sent.strftime(dt_format))
-      print("Body: " + message.body.encode("utf-8"))
-      print
+    print("From: " + name_or_number(msg_from))
+    print("To: " + name_or_number(msg_to))
+    # twilio messages are naive utc dates
+    utc_date_sent = utc.localize(message.date_sent)
+    local_date_sent = utc_date_sent.astimezone(my_timezone)
+    print("Date: " + local_date_sent.strftime(dt_format))
+    print("Body: " + message.body.encode("utf-8"))
+    print
 
